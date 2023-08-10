1d65529df8c2ec1de01673720f510dbc27e5a5d3
killeral <arzzezzan@mail.ru>
2023-08-10 16:54:07 +0300

    Dockerfile & Gradle is problem

--- App/src/main/java/com/arzezan/app/Controller/CatController.java
+++ App/src/main/java/com/arzezan/app/Controller/CatController.java
@@ -4,10 +4,7 @@ import com.arzezan.app.DTO.CatDTO;
 import com.arzezan.app.Entity.Cat;
 import com.arzezan.app.Service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/cats")
@@ -21,7 +18,7 @@ public class CatController {
     }
 
     @PostMapping("saveCat")
-    public Cat save(CatDTO cat) {
+    public Cat save(@RequestBody CatDTO cat) {
         return catService.save(cat);
     }
     @GetMapping("getAll")
--- App/src/main/java/com/arzezan/app/Service/CatService.java
+++ App/src/main/java/com/arzezan/app/Service/CatService.java
@@ -5,7 +5,9 @@ import com.arzezan.app.Entity.Cat;
 import com.arzezan.app.Repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
@@ -18,11 +20,12 @@ public class CatService {
         this.catRepository = catRepository;
     }
 
-    public Cat save(CatDTO catDTO) {
+    public Cat save(@RequestBody CatDTO catDTO) {
         Cat cat = new Cat().
                 setName(catDTO.getName()).
                 setBirthday(catDTO.getBirthday()).
-                setId(String.valueOf(UUID.randomUUID()));
+                setId(String.valueOf(UUID.randomUUID())).
+                setCreatedAt(LocalDateTime.now());
         return catRepository.save(cat);
     }
     public Iterable<Cat> getAll() {
