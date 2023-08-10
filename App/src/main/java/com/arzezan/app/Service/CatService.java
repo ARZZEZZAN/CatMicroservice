3d53e9dc596fb19b0e4ac24a2262c5392856858b
killeral <arzzezzan@mail.ru>
2023-08-10 12:04:18 +0300

    Accessors(chain)

--- App/src/main/java/com/arzezan/app/Service/CatService.java
+++ App/src/main/java/com/arzezan/app/Service/CatService.java
@@ -13,13 +13,16 @@ public class CatService {
 
     @Autowired
     public CatService(CatRepository catRepository) {
-        this.catRepository = catRepository
+        this.catRepository = catRepository;
     }
 
     public Cat save(CatDTO catDTO) {
-        return catRepository.save(catDTO);
+        Cat cat = new Cat().
+                setName(catDTO.getName()).
+                setBirthday(catDTO.getBirthday());
+        return catRepository.save(cat);
     }
-    public Iterable<Cat> getAll() {
-
-    }
+//    public Iterable<Cat> getAll() {
+//
+//    }
 }
--- App/src/main/resources/db/migration/V1_0_0__cat_table.sql
+++ App/src/main/resources/db/migration/V1_0_0__cat_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE cat (
     id VARCHAR(40) PRIMARY KEY not null,
     name VARCHAR(100) NOT NULL,
     birthday DATE NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL default CURRENT_TIMESTAMP
 );
