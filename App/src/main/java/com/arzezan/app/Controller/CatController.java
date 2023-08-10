4deb7d3cfab9b5d350920754748ff20855d483bc
killeral <arzzezzan@mail.ru>
2023-08-10 12:12:09 +0300

    Repository and Controller was updated

--- App/src/main/java/com/arzezan/app/Controller/CatController.java
+++ App/src/main/java/com/arzezan/app/Controller/CatController.java
@@ -2,6 +2,9 @@ package com.arzezan.app.Controller;
 
 import com.arzezan.app.DTO.CatDTO;
 import com.arzezan.app.Entity.Cat;
+import com.arzezan.app.Service.CatService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/cats")
 public class CatController {
-    @PostMapping
+
+    private CatService catService;
+
+    @Autowired
+    public CatController(CatService catService) {
+        this.catService = catService;
+    }
+
+    @PostMapping("saveCat")
     public Cat save(CatDTO cat) {
-        return new Cat();
+        return catService.save(cat);
+    }
+    @GetMapping("getAll")
+    public Iterable<Cat> getAll() {
+        return catService.getAll();
     }
 }
--- App/src/main/java/com/arzezan/app/Service/CatService.java
+++ App/src/main/java/com/arzezan/app/Service/CatService.java
@@ -6,6 +6,8 @@ import com.arzezan.app.Repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class CatService {
 
@@ -19,10 +21,11 @@ public class CatService {
     public Cat save(CatDTO catDTO) {
         Cat cat = new Cat().
                 setName(catDTO.getName()).
-                setBirthday(catDTO.getBirthday());
+                setBirthday(catDTO.getBirthday()).
+                setId(String.valueOf(UUID.randomUUID()));
         return catRepository.save(cat);
     }
-//    public Iterable<Cat> getAll() {
-//
-//    }
+    public Iterable<Cat> getAll() {
+        return catRepository.findAll();
+    }
 }
