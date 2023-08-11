d5c341bf67b107732a1a11a31ff314ab909a759d
killeral <arzzezzan@mail.ru>
2023-08-11 17:28:17 +0300

    Jar file is a big f*cking problem

--- App/build.gradle.kts
+++ App/build.gradle.kts
@@ -2,12 +2,13 @@ plugins {
     java
     id("org.springframework.boot") version "2.6.4"
     id("io.spring.dependency-management") version "1.1.2"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "com.arzezan"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_18
+    sourceCompatibility = JavaVersion.VERSION_11
 }
 
 configurations {
@@ -15,6 +16,7 @@ configurations {
         extendsFrom(configurations.annotationProcessor.get())
     }
 }
+
 
 repositories {
     mavenCentral()
@@ -25,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.flywaydb:flyway-core")
     implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
+    implementation("org.springframework.boot:spring-boot-starter-tomcat")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("org.postgresql:postgresql")
     annotationProcessor("org.projectlombok:lombok")
@@ -34,3 +37,14 @@ dependencies {
 tasks.withType<Test> {
     useJUnitPlatform()
 }
+tasks {
+    val shadowJar by getting(com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar::class) {
+        archiveFileName.set("app-all.jar")
+        manifest {
+            attributes["Main-Class"] = "com.arzezan.app.AppApplication"
+        }
+        configurations = listOf(project.configurations.getByName("runtimeClasspath"))
+    }
+}
+
+
